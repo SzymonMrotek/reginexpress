2c386ff3ad9441abd8212033bed59ebd77ce8477
szymo___on <90240061+SzymonMrotek@users.noreply.github.com>
2023-05-24 14:48:25 +0200

    First Commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/style/style.css">
+    <title>ReginExpress</title>
+</head>
+<body>
+    <header>
+            <img src="/img/malpa.png" alt="Logo">
+            <h1>REGINEXPRESS</h1>
+    </header>
+    <nav>
+        <a href="#">Moje przesyłki</a>
+        <a href="#">Znajdź przesyłkę</a>
+        <a href="#">Moje konto</a>
+        <a href="#">O firmie</a>
+    </nav>
+    <div class="text-section">
+        <div class="text1">
+            Ship With <a class="pink-color">US</a>
+        </div>
+        <div class="text2">
+            for lightning fast delivery!
+        </div>
+        <button class="start-now-button">Start now!</button>
+    </div>
+</body>
+</html>
